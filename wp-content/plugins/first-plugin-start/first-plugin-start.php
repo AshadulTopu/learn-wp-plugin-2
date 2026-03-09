1fb5c111a39bab2996a844bd2531d3da4bc84854
Ashadul Islam <ashadulislam2003@gmail.com>
2026-03-10 01:02:56 +0600

    learn hooks and menu

--- wp-content/plugins/first-plugin-start/first-plugin-start.php
+++ wp-content/plugins/first-plugin-start/first-plugin-start.php
@@ -14,3 +14,16 @@
  * Text Domain:       my-basics-plugin
  * Domain Path:       /languages
  */
+
+
+add_action('admin_menu', 'my_basics_plugin_menu');
+
+function my_basics_plugin_menu()
+{
+    add_menu_page('My Basics Plugin', 'My Basics Plugin', 'manage_options', 'my-basics-plugin', 'my_basics_plugin_page', 'dashicons-admin-generic', 6);
+}
+
+function my_basics_plugin_page()
+{
+    echo '<div class="wrap"><h1>Welcome to My Basics Plugin</h1><p>This is the main page of the plugin.</p></div>';
+}
